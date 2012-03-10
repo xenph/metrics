cba3c31fae8142e1d8457813319e75bdd43ddf57
Christopher Beckett <xenphyan@gmail.com>
2012-03-10 23:05:34 +1100

    Added standard out reporter to assist in building out the API.

---
+++ library/StdOutReporter.php
@@ -0,0 +1,44 @@
+<?php
+class StatsDReporter {
+	function meter() {
+		return new StatsDMeter();
+	}
+	
+	function counter() {
+		return new StatsDCounter();
+	}
+	
+	function timer() {
+		return new StatsDTimer();
+	}
+	
+	function event() {
+		return new StatsDEvent();
+	}	
+}
+
+class StatsDMeter {
+	function mark($options) {
+	}
+}
+
+class StatsDCounter {
+	function increment($options) {
+	}
+	
+	function decrement($options) {
+	}
+}
+
+class StatsDTimer {
+	function time($options) {
+	}
+	
+	function stop($options) {
+	}
+}
+
+class StatsDEvent {
+	function mark($options) {
+	}
+}
