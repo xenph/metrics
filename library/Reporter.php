cfa9ba6ca770f7dd2862e19f27f8087b26f18750
Christopher Beckett <xenphyan@gmail.com>
2012-03-10 23:13:17 +1100

    Wired up the standard out reporter.

--- library/Reporter.php
+++ library/Reporter.php
@@ -1,10 +1,16 @@
 <?php
 require_once('StatsDReporter.php');
+require_once('StdOutReporter.php');
+
 class Reporter {
 	protected $_key_mask;
 	
 	function __construct($key_mask) {
 		$this->_key_mask = $key_mask;
+	}
+	
+	function stdout() {
+		return Metrics::saveReporter($this->_key_mask, new StdOutReporter($options));
 	}
 	
 	function statsd($options = array('host' => 'localhost', 'port' => 1111)) {
