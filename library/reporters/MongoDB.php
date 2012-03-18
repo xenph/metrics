6b2086752d84442c78bcef7294067497e2efd556
Chaitanya Kuber <chaitanya.kuber@gmail.com>
2012-03-19 07:46:16 +1100

    started working on a mongodb endpoint for metrics

--- library/reporters/MongoDB.php
+++ library/reporters/MongoDB.php
@@ -1,12 +1,46 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: chaitanya
- * Date: 17/03/12
- * Time: 8:10 PM
- * To change this template use File | Settings | File Templates.
- */
-class MongoDB
-{
+namespace metrics\reporters;
 
+class MongoDB implements \metrics\reports\IMetrics
+{
+    private $mongo;
+    private $db;
+    private $collections;
+
+    public function __construct()
+    {
+        $this->mongo = new \Mongo();
+        $this->db = $this->mongo->metrics;
+        $this->collections = array(
+            'events' => $this->db->events,
+            'counters' => $this->db->counters,
+            );
+    }
+
+    public function meter($key)
+    {
+
+    }
+
+    public function counter($key)
+    {
+
+    }
+
+    public function timer($key)
+    {
+
+    }
+
+    public function event($key)
+    {
+        // TODO: Implement event() method.
+    }
+
+    private function write($data)
+    {
+        /** @var $events  */
+        $events = $this->collections['events'];
+        //$events;
+    }
 }
